edea53d5f05fa2ae15bb4c5c9283e22865f4aacc
Elmer de Looff <elmer.delooff@gmail.com>
2013-03-22 18:53:00 +0100

    Rewrote a few functions to be more self-explaining.
    Functions now adhere to a camelCase typing which was mostly but not strictly adhered.
    
    LPD8806.show method improved for better readability.

--- colors.py
+++ colors.py
@@ -5,106 +5,109 @@ import time
 import random
 
 
-# Slightly different, this one makes the rainbow wheel equally distributed
-# along the chain
-def RainbowCycle(strip):
-  for j in xrange(383*5): #5 cycles of all 384 colors in the wheel
-    for i in xrange(strip.led_count):
-      # tricky math! we use each pixel as a fraction of the full 384-color wheel
-      # (thats the i / strip.led_count() part)
-      # Then add in j which makes the colors go around per pixel
-      # the % 384 is to make the wheel cycle around
-      colors = Wheel( ((i * 384 / strip.led_count) + j) % 384)
-      strip.setPixelColor(i, *colors)
-    strip.show() # write all the pixels out
+def rainbowCycle(strip, repeats=5):
+  """Perform a quick rainbow cycle."""
+  for _repeat in range(repeats):
+    for angle in range(384):
+      for led in range(strip.led_count):
+        # We use each pixel as a fraction of the full 384-color wheel
+        # (thats the led / strip.led_count() part)
+        # Then add in the angle which makes the colors go around per pixel
+        # the % 384 is to make the wheel cycle around
+        colors = wheel( ((led * 384 / strip.led_count) + angle) % 384)
+        strip.setPixelColor(led, *colors)
+      strip.show() # write all the pixels out
 
-def Disco(strip, colors, segments, delay):
+
+def disco(strip, colors, segments, delay):
+  """Place random colors on segments of the LED-strip."""
   segmentlength = strip.led_count / segments
-  for i in xrange(strip.led_count):
-    if i % segmentlength == 0:
+  for led in range(strip.led_count):
+    if not led % segmentlength:
       color = random.choice(colors)
-    strip.setPixelColor(i, *color)
+    strip.setPixelColor(led, *color)
   strip.show()
   time.sleep(delay)
 
-def colorWipe(strip, r, g, b):
-  for i in xrange(strip.led_count):
-    strip.setPixelColor(i, r, g, b)
+
+def colorWipe(strip, r, g, b, delay=0.2):
+  """Fills the strip from begin to end with a single color."""
+  for led in range(strip.led_count):
+    strip.setPixelColor(led, r, g, b)
     strip.show()
-    time.sleep(.2)
+    time.sleep(delay)
 
-def colorChase(strip, r, g, b):
-  # Chase one dot down the full strip.
 
+def colorChase(strip, r, g, b, delay=0.2):
+  """Chase one dot down the full strip."""
   # Start by turning all pixels off:
-  for i in xrange(strip.led_count):
-    strip.setPixelColor(i, 0, 0, 0)
+  for led in range(strip.led_count):
+    strip.setPixelColor(led, 0, 0, 0)
 
   # Then display one pixel at a time:
-  for i in xrange(strip.led_count):
-    strip.setPixelColor(i, r, g, b) # Set new pixel 'on'
-    strip.show()                  # Refresh LED states
-    strip.setPixelColor(i, 0, 0, 0) # Erase pixel, but don't refresh!
-    time.sleep(.2)
+  for led in range(strip.led_count):
+    strip.setPixelColor(led, r, g, b) # Set new pixel 'on'
+    strip.show()                      # Refresh LED states
+    strip.setPixelColor(led, 0, 0, 0) # Erase pixel, but don't refresh!
+    time.sleep(delay)
   strip.show() # Refresh to turn off last pixel
 
-
 def rainbow(strip):
-  for j in xrange(384): # 3 cycles of all 384 colors in the wheel
-    for i in xrange(strip.led_count):
-      colors = Wheel((i + j) % 384)
-      strip.setPixelColor(i, *colors)
+  for angle in range(384): # Cycle over all colors in the 384 'degree' wheel
+    for led in range(strip.led_count):
+      color = wheel((led + angle) % 384)
+      strip.setPixelColor(led, *color)
     strip.show()
 
-def Wheel(WheelPos):
-  if WheelPos / 128 == 0:
-    r = 127 - WheelPos % 128    # Red down
-    g = WheelPos % 128          # Green up
-    b = 0                       # blue off
-  elif WheelPos / 128 == 1:
-    g = 127 - WheelPos % 128    # green down
-    b = WheelPos % 128          # blue up
-    r = 0                       #red off
-  elif WheelPos / 128 == 2:
-    b = 127 - WheelPos % 128    # blue down
-    r = WheelPos % 128          # red up
-    g = 0                       # green off
+def wheel(angle):
+  if angle / 128 == 0:
+    r = 127 - angle % 128    # Red down
+    g = angle % 128          # Green up
+    b = 0                    # blue off
+  elif angle / 128 == 1:
+    g = 127 - angle % 128    # green down
+    b = angle % 128          # blue up
+    r = 0                    #red off
+  elif angle / 128 == 2:
+    b = 127 - angle % 128    # blue down
+    r = angle % 128          # red up
+    g = 0                    # green off
   return r, g, b
 
-class LPD8806(object):
 
+class LPD8806(object):
+  """LPD8806 RGB strip abstraction."""
   def __init__(self, host, port, led_count):
     self.host = host
     self.port = port
     self.led_count = led_count
     self.leds = {}
 
-  def setPixelColor(self, led, r, g, b):
-    self.leds[led] = (r, g, b)
+  def setPixelColor(self, led, red, green, blue):
+    """Sets the red, green and blue levels for a particular pixel."""
+    self.leds[led] = red, green, blue
 
   def show(self):
+    """Writes out all pixels to the ethernet listener."""
     output = []
     try:
-      for led in xrange(self.led_count):
-        output.append(chr(self.leds[led][0] + 65))
-        output.append(chr(self.leds[led][1] + 65))
-        output.append(chr(self.leds[led][2] + 65))
-        print (chr(self.leds[led][0] + 65) +
-               chr(self.leds[led][1] + 65) +
-               chr(self.leds[led][2] + 65))
+      for led in range(self.led_count):
+        for channel in self.leds[led]:
+          output.append(chr(channel + 65))
+        print ''.join(output[-3:])
     except KeyError:
+      # No color is defined, set the pixel to black
       output.append('AAA')
-
     t = telnetlib.Telnet(self.host, self.port)
     t.write(''.join(output))
     t.close()
 
 
-def ForeverRainbows(host, port, led_count):
+def foreverRainbow(host, port, led_count):
   """Runs a changing color rainbow until interrupted."""
   strip = LPD8806(host, port, led_count)
   while True:
-    RainbowCycle(strip)
+    rainbowCycle(strip)
 
 
 def main():
@@ -118,7 +121,7 @@ def main():
   parser.add_option('-l', '--leds', type='int', default=160,
                     help='Number of leds to drive.')
   options, _arguments = parser.parse_args()
-  ForeverRainbows(options.host, options.port, options.leds)
+  foreverRainbow(options.host, options.port, options.leds)
 
 
 if __name__ == '__main__':
